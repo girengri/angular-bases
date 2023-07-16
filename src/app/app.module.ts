9374d6817c7daac7ab8a55978840cd7d9094c567
Giovany Rendon <giorengri.96@yahoo.com>
2023-07-16 17:28:26 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { CounterModule } from './counter/counter.module';
+import { DbzModule } from './dbz/dbz.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 import { AppComponent } from './app.component';
 
-import { HeroesModule } from './heroes/heroes.module';
-
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CounterModule, HeroesModule],
+  imports: [BrowserModule, CounterModule, DbzModule, HeroesModule],
   providers: [],
   bootstrap: [AppComponent],
 })
